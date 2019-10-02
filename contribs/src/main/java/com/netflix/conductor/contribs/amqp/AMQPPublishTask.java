e6fe47b0e169ecf0bd5acd6487dc9a563b51dd1a
Harold Hannon <haroldhannon@gmail.com>
2019-10-02 18:55:00 -0500

    Update AMQPPublishTask.java

--- contribs/src/main/java/com/netflix/conductor/contribs/amqp/AMQPPublishTask.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/amqp/AMQPPublishTask.java
@@ -67,17 +67,21 @@ public class AMQPPublishTask extends WorkflowSystemTask {
 	}
 
 	private static ObjectMapper objectMapper() {
+		System.out.println("******************************* mapper start");
 		final ObjectMapper om = new ObjectMapper();
 		om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		om.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
 		om.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
 		om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 		om.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+		System.out.println("******************************* mapper stop");
 		return om;
 	}
 
 	@Override
 	public void start(Workflow workflow, Task task, WorkflowExecutor executor) {
+		
+		System.out.println("******************************* start publish ");
 
 		long taskStartMillis = Instant.now().toEpochMilli();
 		task.setWorkerId(config.getServerId());
@@ -104,19 +108,27 @@ public class AMQPPublishTask extends WorkflowSystemTask {
 			this.factory.setPassword("conductor");
 			logger.info("AMQP Connection Factory initialized...");
 			this.factory.setHost(this.input.getHosts());
+			
+			System.out.println("*******************************factory done");
 
 			this.connection = factory.newConnection();
+			
+			System.out.println("*******************************connection made");
+			
 			com.rabbitmq.client.Channel channel = connection.createChannel();
 			channel.queueDeclare(this.input.getQueue(), true, false, false, null);
+			System.out.println("*******************************channel made");
 
 			channel.basicPublish("", this.input.getQueue(), MessageProperties.PERSISTENT_TEXT_PLAIN,
 					this.input.getValue().getBytes("UTF-8"));
+			System.out.println("*******************************published");
 
 			task.setStatus(Task.Status.COMPLETED);
 			long timeTakenToCompleteTask = Instant.now().toEpochMilli() - taskStartMillis;
 			logger.info("Published message {}, Time taken {}", input, timeTakenToCompleteTask);
 
 		} catch (Exception e) {
+			System.out.println(e.printStackTrace());
 			logger.error(String.format("Failed to invoke amqp task for input {} - unknown exception: {}", input), e);
 			markTaskAsFailed(task, FAILED_TO_INVOKE + e.getMessage());
 		}
