ee4cf471bab85ff2336468b6c7963b16513c8ffe
Harold Hannon <haroldhannon@gmail.com>
2019-10-31 15:15:14 -0500

    Update CoreModule.java

--- core/src/main/java/com/netflix/conductor/core/config/CoreModule.java
+++ core/src/main/java/com/netflix/conductor/core/config/CoreModule.java
@@ -39,6 +39,7 @@ import com.netflix.conductor.core.execution.mapper.ForkJoinTaskMapper;
 import com.netflix.conductor.core.execution.mapper.HTTPTaskMapper;
 import com.netflix.conductor.core.execution.mapper.JoinTaskMapper;
 import com.netflix.conductor.core.execution.mapper.AMQPPublishTaskMapper;
+import com.netflix.conductor.core.execution.mapper.AMQPWaitTaskMapper;
 import com.netflix.conductor.core.execution.mapper.KafkaPublishTaskMapper;
 import com.netflix.conductor.core.execution.mapper.LambdaTaskMapper;
 import com.netflix.conductor.core.execution.mapper.SimpleTaskMapper;
@@ -67,6 +68,7 @@ import static com.netflix.conductor.common.metadata.workflow.TaskType.TASK_TYPE_
 import static com.netflix.conductor.common.metadata.workflow.TaskType.TASK_TYPE_HTTP;
 import static com.netflix.conductor.common.metadata.workflow.TaskType.TASK_TYPE_JOIN;
 import static com.netflix.conductor.common.metadata.workflow.TaskType.TASK_TYPE_AMQP_PUBLISH;
+import static com.netflix.conductor.common.metadata.workflow.TaskType.TASK_TYPE_AMQP_WAIT;
 import static com.netflix.conductor.common.metadata.workflow.TaskType.TASK_TYPE_KAFKA_PUBLISH;
 import static com.netflix.conductor.common.metadata.workflow.TaskType.TASK_TYPE_LAMBDA;
 import static com.netflix.conductor.common.metadata.workflow.TaskType.TASK_TYPE_SIMPLE;
@@ -75,178 +77,186 @@ import static com.netflix.conductor.common.metadata.workflow.TaskType.TASK_TYPE_
 import static com.netflix.conductor.common.metadata.workflow.TaskType.TASK_TYPE_USER_DEFINED;
 import static com.netflix.conductor.common.metadata.workflow.TaskType.TASK_TYPE_WAIT;
 import static com.netflix.conductor.core.events.EventQueues.EVENT_QUEUE_PROVIDERS_QUALIFIER;
+
 /**
  * @author Viren
  */
 public class CoreModule extends AbstractModule {
 
-    private static final String CONDUCTOR_QUALIFIER = "conductor";
-    private static final String TASK_MAPPERS_QUALIFIER = "TaskMappers";
+	private static final String CONDUCTOR_QUALIFIER = "conductor";
+	private static final String TASK_MAPPERS_QUALIFIER = "TaskMappers";
 
-    @Override
-    protected void configure() {
-        install(MultibindingsScanner.asModule());
-        bind(SystemTaskWorkerCoordinator.class).asEagerSingleton();
-        bind(SubWorkflow.class).asEagerSingleton();
-        bind(Wait.class).asEagerSingleton();
-        bind(Event.class).asEagerSingleton();
-        bind(Lambda.class).asEagerSingleton();
-        bind(Terminate.class).asEagerSingleton();
-        bind(IsolatedTaskQueueProducer.class).asEagerSingleton();
+	@Override
+	protected void configure() {
+		install(MultibindingsScanner.asModule());
+		bind(SystemTaskWorkerCoordinator.class).asEagerSingleton();
+		bind(SubWorkflow.class).asEagerSingleton();
+		bind(Wait.class).asEagerSingleton();
+		bind(Event.class).asEagerSingleton();
+		bind(Lambda.class).asEagerSingleton();
+		bind(Terminate.class).asEagerSingleton();
+		bind(IsolatedTaskQueueProducer.class).asEagerSingleton();
 
-        // start processing events when instance starts
-        bind(ActionProcessor.class).to(SimpleActionProcessor.class);
-        bind(EventProcessor.class).to(SimpleEventProcessor.class).asEagerSingleton();
-    }
+		// start processing events when instance starts
+		bind(ActionProcessor.class).to(SimpleActionProcessor.class);
+		bind(EventProcessor.class).to(SimpleEventProcessor.class).asEagerSingleton();
+	}
 
-    @Provides
-    @Singleton
-    public ParametersUtils getParameterUtils() {
-        return new ParametersUtils();
-    }
+	@Provides
+	@Singleton
+	public ParametersUtils getParameterUtils() {
+		return new ParametersUtils();
+	}
 
-    @Provides
-    @Singleton
-    public JsonUtils getJsonUtils() {
-        return new JsonUtils();
-    }
+	@Provides
+	@Singleton
+	public JsonUtils getJsonUtils() {
+		return new JsonUtils();
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(CONDUCTOR_QUALIFIER)
-    @Singleton
-    @Named(EVENT_QUEUE_PROVIDERS_QUALIFIER)
-    public EventQueueProvider getDynoEventQueueProvider(QueueDAO queueDAO, Configuration configuration) {
-        return new DynoEventQueueProvider(queueDAO, configuration);
-    }
+	@ProvidesIntoMap
+	@StringMapKey(CONDUCTOR_QUALIFIER)
+	@Singleton
+	@Named(EVENT_QUEUE_PROVIDERS_QUALIFIER)
+	public EventQueueProvider getDynoEventQueueProvider(QueueDAO queueDAO, Configuration configuration) {
+		return new DynoEventQueueProvider(queueDAO, configuration);
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_DECISION)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getDecisionTaskMapper() {
-        return new DecisionTaskMapper();
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_DECISION)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getDecisionTaskMapper() {
+		return new DecisionTaskMapper();
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_DYNAMIC)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getDynamicTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
-        return new DynamicTaskMapper(parametersUtils, metadataDAO);
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_DYNAMIC)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getDynamicTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
+		return new DynamicTaskMapper(parametersUtils, metadataDAO);
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_JOIN)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getJoinTaskMapper() {
-        return new JoinTaskMapper();
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_JOIN)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getJoinTaskMapper() {
+		return new JoinTaskMapper();
+	}
 
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_FORK_JOIN_DYNAMIC)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getForkJoinDynamicTaskMapper(ParametersUtils parametersUtils, ObjectMapper objectMapper,
+			MetadataDAO metadataDAO) {
+		return new ForkJoinDynamicTaskMapper(parametersUtils, objectMapper, metadataDAO);
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_FORK_JOIN_DYNAMIC)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getForkJoinDynamicTaskMapper(ParametersUtils parametersUtils, ObjectMapper objectMapper, MetadataDAO metadataDAO) {
-        return new ForkJoinDynamicTaskMapper(parametersUtils, objectMapper, metadataDAO);
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_EVENT)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getEventTaskMapper(ParametersUtils parametersUtils) {
+		return new EventTaskMapper(parametersUtils);
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_EVENT)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getEventTaskMapper(ParametersUtils parametersUtils) {
-        return new EventTaskMapper(parametersUtils);
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_WAIT)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getWaitTaskMapper(ParametersUtils parametersUtils) {
+		return new WaitTaskMapper(parametersUtils);
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_WAIT)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getWaitTaskMapper(ParametersUtils parametersUtils) {
-        return new WaitTaskMapper(parametersUtils);
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_SUB_WORKFLOW)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getSubWorkflowTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
+		return new SubWorkflowTaskMapper(parametersUtils, metadataDAO);
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_SUB_WORKFLOW)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getSubWorkflowTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
-        return new SubWorkflowTaskMapper(parametersUtils, metadataDAO);
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_FORK_JOIN)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getForkJoinTaskMapper() {
+		return new ForkJoinTaskMapper();
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_FORK_JOIN)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getForkJoinTaskMapper() {
-        return new ForkJoinTaskMapper();
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_USER_DEFINED)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getUserDefinedTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
+		return new UserDefinedTaskMapper(parametersUtils, metadataDAO);
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_USER_DEFINED)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getUserDefinedTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
-        return new UserDefinedTaskMapper(parametersUtils, metadataDAO);
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_SIMPLE)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getSimpleTaskMapper(ParametersUtils parametersUtils) {
+		return new SimpleTaskMapper(parametersUtils);
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_SIMPLE)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getSimpleTaskMapper(ParametersUtils parametersUtils) {
-        return new SimpleTaskMapper(parametersUtils);
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_HTTP)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getHTTPTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
+		return new HTTPTaskMapper(parametersUtils, metadataDAO);
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_HTTP)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getHTTPTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
-        return new HTTPTaskMapper(parametersUtils, metadataDAO);
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_LAMBDA)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getLambdaTaskMapper(ParametersUtils parametersUtils) {
+		return new LambdaTaskMapper(parametersUtils);
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_LAMBDA)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getLambdaTaskMapper(ParametersUtils parametersUtils) {
-        return new LambdaTaskMapper(parametersUtils);
-    }
-    
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_EXCLUSIVE_JOIN)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getExclusiveJoinTaskMapper() {
-        return new ExclusiveJoinTaskMapper();
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_EXCLUSIVE_JOIN)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getExclusiveJoinTaskMapper() {
+		return new ExclusiveJoinTaskMapper();
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_TERMINATE)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getTerminateTaskMapper(ParametersUtils parametersUtils) {
-        return new TerminateTaskMapper(parametersUtils);
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_TERMINATE)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getTerminateTaskMapper(ParametersUtils parametersUtils) {
+		return new TerminateTaskMapper(parametersUtils);
+	}
 
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_KAFKA_PUBLISH)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getKafkaPublishTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
-        return new KafkaPublishTaskMapper(parametersUtils, metadataDAO);
-    }
-    
-    @ProvidesIntoMap
-    @StringMapKey(TASK_TYPE_AMQP_PUBLISH)
-    @Singleton
-    @Named(TASK_MAPPERS_QUALIFIER)
-    public TaskMapper getAMQPPublishTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
-        System.out.println("*********************start core module get task mapper");
-        return new AMQPPublishTaskMapper(parametersUtils, metadataDAO);
-    }
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_KAFKA_PUBLISH)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getKafkaPublishTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
+		return new KafkaPublishTaskMapper(parametersUtils, metadataDAO);
+	}
+
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_AMQP_PUBLISH)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getAMQPPublishTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
+		return new AMQPPublishTaskMapper(parametersUtils, metadataDAO);
+	}
+
+	@ProvidesIntoMap
+	@StringMapKey(TASK_TYPE_AMQP_WAIT)
+	@Singleton
+	@Named(TASK_MAPPERS_QUALIFIER)
+	public TaskMapper getAMQPWaitTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
+		return new AMQPWaitTaskMapper(parametersUtils, metadataDAO);
+	}
 
 }
