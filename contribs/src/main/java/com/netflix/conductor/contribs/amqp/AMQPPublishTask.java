f48980475e56cbccd598db893f3cfd8b190a4a68
Harold Hannon <haroldhannon@gmail.com>
2019-11-07 18:36:51 -0600

    Update AMQPPublishTask.java

--- contribs/src/main/java/com/netflix/conductor/contribs/amqp/AMQPPublishTask.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/amqp/AMQPPublishTask.java
@@ -1,77 +1,250 @@
-package com.netflix.conductor.core.execution.mapper;
+package com.netflix.conductor.contribs.amqp;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.annotations.VisibleForTesting;
 import com.netflix.conductor.common.metadata.tasks.Task;
-import com.netflix.conductor.common.metadata.tasks.TaskDef;
-import com.netflix.conductor.common.metadata.workflow.TaskType;
-import com.netflix.conductor.common.metadata.workflow.WorkflowDef;
-import com.netflix.conductor.common.metadata.workflow.WorkflowTask;
 import com.netflix.conductor.common.run.Workflow;
-import com.netflix.conductor.core.execution.ParametersUtils;
-import com.netflix.conductor.core.execution.TerminateWorkflowException;
-import com.netflix.conductor.dao.MetadataDAO;
+import com.netflix.conductor.core.config.Configuration;
+import com.netflix.conductor.core.execution.WorkflowExecutor;
+import com.netflix.conductor.core.execution.tasks.WorkflowSystemTask;
+import com.rabbitmq.client.AMQP.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import org.apache.commons.lang3.StringUtils;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
-import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
-public class AMQPPublishTaskMapper implements TaskMapper  {
-	public static final Logger logger = LoggerFactory.getLogger(AMQPPublishTaskMapper.class);
+@Singleton
+public class AMQPPublishTask extends WorkflowSystemTask {
 
-	private final ParametersUtils parametersUtils;
-	private final MetadataDAO metadataDAO;
+	static final String REQUEST_PARAMETER_NAME = "amqp_request";
+	private static final String NAME = "AMQP_PUBLISH";
+	private static final String MISSING_REQUEST = "Missing AMQP request. Task input MUST have a '"
+			+ REQUEST_PARAMETER_NAME
+			+ "' key with AMQPTask.Input as value. See documentation for AMQPTask for required input parameters";
+	private static final String MISSING_HOST_SERVERS = "No boot strap servers specified";
+	private static final String MISSING_AMQP_QUEUE = "Missing AMQP topic. See documentation for AMQPTask for required input parameters";
+	private static final String MISSING_AMQP_VALUE = "Missing AMQP value.  See documentation for AMQPTask for required input parameters";
+	private static final String MISSING_AMQP_USERNAME = "Missing AMQP username.  See documentation for AMQPTask for required input parameters";
+	private static final String MISSING_AMQP_PASSWORD = "Missing AMQP password.  See documentation for AMQPTask for required input parameters";
+	private static final String FAILED_TO_INVOKE = "Failed to invoke AMQP task due to: ";
 
-	public AMQPPublishTaskMapper(ParametersUtils parametersUtils, MetadataDAO metadataDAO) {
-		this.parametersUtils = parametersUtils;
-		this.metadataDAO = metadataDAO;
+	private ObjectMapper om = objectMapper();
+	private Configuration config;
+	private String requestParameter;
+	AMQPProducerManager producerManager;
+	private ConnectionFactory factory;
+	private Connection connection;
+	private AMQPPublishTask.Input input;
+
+	private static final Logger logger = LoggerFactory.getLogger(AMQPPublishTask.class);
+
+	@Inject
+	public AMQPPublishTask(Configuration config, AMQPProducerManager clientManager) {
+		super(NAME);
+		this.config = config;
+		this.requestParameter = REQUEST_PARAMETER_NAME;
+		this.producerManager = clientManager;
+		logger.info("AMQPTask initialized...");
+		
+
 	}
 
-	/**
-	 * This method maps a {@link WorkflowTask} of type {@link TaskTyp#AMQP_PUBLISH}
-	 * to a {@link Task} in a {@link Task.Status#SCHEDULED} state
-	 *
-	 * @param taskMapperContext: A wrapper class containing the {@link WorkflowTask}, {@link WorkflowDef}, {@link Workflow} and a string representation of the TaskId
-	 * @return a List with just one AMQP task
-	 * @throws TerminateWorkflowException In case if the task definition does not exist
-	 */
+	private static ObjectMapper objectMapper() {
+		
+		final ObjectMapper om = new ObjectMapper();
+		om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		om.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
+		om.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+		om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		om.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+		
+		return om;
+	}
+
 	@Override
-	public List<Task> getMappedTasks(TaskMapperContext taskMapperContext) throws TerminateWorkflowException {
+	public void start(Workflow workflow, Task task, WorkflowExecutor executor) {
 
-		logger.debug("TaskMapperContext {} in AMQPPublishTaskMapper", taskMapperContext);
+		
 
-		WorkflowTask taskToSchedule = taskMapperContext.getTaskToSchedule();
-		Workflow workflowInstance = taskMapperContext.getWorkflowInstance();
-		String taskId = taskMapperContext.getTaskId();
-		int retryCount = taskMapperContext.getRetryCount();
+		long taskStartMillis = Instant.now().toEpochMilli();
+		
+		task.setWorkerId(config.getServerId());
+		
+		Object request = task.getInputData().get(requestParameter);
+		
 
-		TaskDef taskDefinition = Optional.ofNullable(taskMapperContext.getTaskDefinition())
-						.orElseGet(() -> Optional.ofNullable(metadataDAO.getTaskDef(taskToSchedule.getName()))
-						.orElse(null));
-
-		Map<String, Object> input = parametersUtils.getTaskInputV2(taskToSchedule.getInputParameters(), workflowInstance, taskId, taskDefinition);
-
-		Task amqpPublishTask = new Task();
-		amqpPublishTask.setTaskType(taskToSchedule.getType());
-		amqpPublishTask.setTaskDefName(taskToSchedule.getName());
-		amqpPublishTask.setReferenceTaskName(taskToSchedule.getTaskReferenceName());
-		amqpPublishTask.setWorkflowInstanceId(workflowInstance.getWorkflowId());
-		amqpPublishTask.setWorkflowType(workflowInstance.getWorkflowName());
-		amqpPublishTask.setCorrelationId(workflowInstance.getCorrelationId());
-		amqpPublishTask.setScheduledTime(System.currentTimeMillis());
-		amqpPublishTask.setTaskId(taskId);
-		amqpPublishTask.setInputData(input);
-		amqpPublishTask.setStatus(Task.Status.SCHEDULED);
-		amqpPublishTask.setRetryCount(retryCount);
-		amqpPublishTask.setCallbackAfterSeconds(taskToSchedule.getStartDelay());
-		amqpPublishTask.setWorkflowTask(taskToSchedule);
-		amqpPublishTask.setWorkflowPriority(workflowInstance.getPriority());
-		if (Objects.nonNull(taskDefinition)) {
-			amqpPublishTask.setRateLimitPerFrequency(taskDefinition.getRateLimitPerFrequency());
-			amqpPublishTask.setRateLimitFrequencyInSeconds(taskDefinition.getRateLimitFrequencyInSeconds());
+		if (Objects.isNull(request)) {
+			markTaskAsFailed(task, MISSING_REQUEST);
+			
+			return;
 		}
-		return Collections.singletonList(amqpPublishTask);
+
+		AMQPPublishTask.Input input = om.convertValue(request, AMQPPublishTask.Input.class);
+
+		
+
+		if (StringUtils.isBlank(input.getQueue())) {
+			markTaskAsFailed(task, MISSING_AMQP_QUEUE);
+			return;
+		}
+
+		if (StringUtils.isBlank(input.getUserName())) {
+			markTaskAsFailed(task, MISSING_AMQP_USERNAME);
+			return;
+		}
+
+		if (StringUtils.isBlank(input.getPassword())) {
+			markTaskAsFailed(task, MISSING_AMQP_PASSWORD);
+			return;
+		}
+
+		if (Objects.isNull(input.getValue())) {
+			markTaskAsFailed(task, MISSING_AMQP_VALUE);
+			return;
+		}
+
+		
+		try {
+			this.factory = new ConnectionFactory();
+			this.factory.setUsername(input.getUserName());
+			this.factory.setPassword(input.getPassword());
+			logger.info("AMQP Connection Factory initialized...");
+			this.factory.setHost(input.getHosts());
+
+			
+			this.connection = factory.newConnection();
+
+			
+			com.rabbitmq.client.Channel channel = connection.createChannel();
+			channel.queueDeclare(input.getQueue(), true, false, false, null);
+			
+			channel.basicPublish("", input.getQueue(), MessageProperties.PERSISTENT_TEXT_PLAIN,
+					input.getValue().getBytes("UTF-8"));
+			task.setStatus(Task.Status.COMPLETED);
+			long timeTakenToCompleteTask = Instant.now().toEpochMilli() - taskStartMillis;
+			logger.info("Published message {}, Time taken {}", input, timeTakenToCompleteTask);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(String.format("Failed to invoke amqp task for input {} - unknown exception: {}", input), e);
+			markTaskAsFailed(task, FAILED_TO_INVOKE + e.getMessage());
+		}
+	}
+
+	private void markTaskAsFailed(Task task, String reasonForIncompletion) {
+		task.setReasonForIncompletion(reasonForIncompletion);
+		task.setStatus(Task.Status.FAILED);
+	}
+
+	@Override
+	public boolean execute(Workflow workflow, Task task, WorkflowExecutor executor) {
+		return false;
+	}
+
+	@Override
+	public void cancel(Workflow workflow, Task task, WorkflowExecutor executor) {
+		task.setStatus(Task.Status.CANCELED);
+	}
+
+	@Override
+	public boolean isAsync() {
+		return false;
+	}
+
+	public static class Input {
+
+		private Map<String, Object> headers = new HashMap<>();
+
+		private String hosts;
+
+		private String value;
+
+		private Integer requestTimeoutMs;
+
+		private String queue;
+
+		private String userName;
+
+		private String password;
+
+		public Map<String, Object> getHeaders() {
+			return headers;
+		}
+
+		public void setHeaders(Map<String, Object> headers) {
+			this.headers = headers;
+		}
+
+		public String getHosts() {
+			return hosts;
+		}
+
+		public void setHosts(String hosts) {
+			this.hosts = hosts;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+		public Integer getRequestTimeoutMs() {
+			return requestTimeoutMs;
+		}
+
+		public void setRequestTimeoutMs(Integer requestTimeoutMs) {
+			this.requestTimeoutMs = requestTimeoutMs;
+		}
+
+		public String getQueue() {
+			return queue;
+		}
+
+		public void setQueue(String queue) {
+			this.queue = queue;
+		}
+
+		public String getUserName() {
+			return userName;
+		}
+
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		@Override
+		public String toString() {
+			return "";
+		}
 	}
 }
