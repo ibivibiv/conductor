3214c990b891e2c61f75fefb47df8e307f802594
Harold Hannon <haroldhannon@gmail.com>
2019-11-23 11:19:04 -0600

    Update AMQPWaitTask.java

--- contribs/src/main/java/com/netflix/conductor/contribs/amqp/AMQPWaitTask.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/amqp/AMQPWaitTask.java
@@ -187,7 +187,7 @@ public class AMQPWaitTask extends WorkflowSystemTask {
 			RerunWorkflowRequest rerun = new RerunWorkflowRequest();
 			rerun.setReRunFromWorkflowId(task.getWorkflowInstanceId());
 			rerun.setReRunFromTaskId(task.getTaskId());
-			rerun.setWorkflowInput("");
+			rerun.setWorkflowInput(new HashMap<String, Object>());
 			executor.rerun(rerun);
 		}
 
