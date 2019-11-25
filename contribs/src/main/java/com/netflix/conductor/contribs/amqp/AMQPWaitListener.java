5ce4839721f864a9e351c721b8f7e83e9a08fb00
Harold Hannon <haroldhannon@gmail.com>
2019-11-25 16:21:06 -0600

    Update AMQPWaitListener.java

--- contribs/src/main/java/com/netflix/conductor/contribs/amqp/AMQPWaitListener.java
+++ contribs/src/main/java/com/netflix/conductor/contribs/amqp/AMQPWaitListener.java
@@ -1,5 +1,10 @@
 package com.netflix.conductor.contribs.amqp;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.netflix.conductor.core.execution.WorkflowExecutorModule;
+import com.netflix.conductor.service.TaskService;
+import com.netflix.conductor.service.TaskServiceImpl;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -9,6 +14,8 @@ public class AMQPWaitListener {
 
 	public AMQPWaitListener() {
 
+		Injector injector = Guice.createInjector(new WorkflowExecutorModule());
+		TaskService taskService = injector.getInstance(TaskServiceImpl.class);
 		Thread listener = new Thread(new Listener());
 		listener.start();
 
@@ -19,29 +26,27 @@ public class AMQPWaitListener {
 		@Override
 		public void run() {
 
-			while (true) {
+			try {
 
-				try {
-					ConnectionFactory factory = new ConnectionFactory();
-					factory.setHost("rabbitmq-headless");
-					factory.setUsername("conductor");
-					factory.setPassword("conductor");
-					Connection connection = factory.newConnection();
-					Channel channel = connection.createChannel();
+				ConnectionFactory factory = new ConnectionFactory();
+				factory.setHost("rabbitmq-headless");
+				factory.setUsername("conductor");
+				factory.setPassword("conductor");
+				Connection connection = factory.newConnection();
+				Channel channel = connection.createChannel();
 
-					channel.queueDeclare("conductor", false, false, false, null);
-					System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+				channel.queueDeclare("conductor", false, false, false, null);
+				System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-					DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-						String message = new String(delivery.getBody(), "UTF-8");
-						System.out.println(" [x] Received '" + message + "'");
-					};
-					channel.basicConsume("conductor", true, deliverCallback, consumerTag -> {
-					});
+				DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+					String message = new String(delivery.getBody(), "UTF-8");
+					System.out.println(" [x] Received '" + message + "'");
+				};
+				channel.basicConsume("conductor", true, deliverCallback, consumerTag -> {
+				});
 
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 
 		}
