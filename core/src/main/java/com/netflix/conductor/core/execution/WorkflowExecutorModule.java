9d2f3cbaacbe2c406d040f653f036cb9ead48932
Harold Hannon <haroldhannon@gmail.com>
2019-11-26 08:47:18 -0600

    Update WorkflowExecutorModule.java

--- core/src/main/java/com/netflix/conductor/core/execution/WorkflowExecutorModule.java
+++ core/src/main/java/com/netflix/conductor/core/execution/WorkflowExecutorModule.java
@@ -1,6 +1,7 @@
 package com.netflix.conductor.core.execution;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 import com.netflix.conductor.service.AdminService;
 import com.netflix.conductor.service.AdminServiceImpl;
 import com.netflix.conductor.service.EventService;
@@ -27,7 +28,8 @@ public class WorkflowExecutorModule extends AbstractModule {
         bind(AdminService.class).to(AdminServiceImpl.class);
         bind(WorkflowService.class).to(WorkflowServiceImpl.class);
         bind(WorkflowBulkService.class).to(WorkflowBulkServiceImpl.class);
-        bind(TaskService.class).to(TaskServiceImpl.class);
+        bind(TaskService.class).annotatedWith(Names.named("taskService")).to(TaskServiceImpl.class);
+        expose(TaskService.class).annotatedWith(Names.named("taskService"));
         bind(EventService.class).to(EventServiceImpl.class);
         bind(MetadataService.class).to(MetadataServiceImpl.class);
     }
